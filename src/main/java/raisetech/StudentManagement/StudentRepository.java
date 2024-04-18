e65a92c19353c8de8f8abff3eb47c4c0da161929
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2024-04-18 12:55:54 +0900

    ブランチでのコミット

---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,24 @@
+package raisetech.StudentManagement;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+  @Insert("INSERT INTO student values(#{name}, #{age})")
+  void registerStudent(String name, int age);
+
+  @Update("UPDATE student set age = #{age} WHERE name = #{name}")
+  void updateStudent(String name, int age);
+
+  @Delete("DELETE FROM student WHERE name = #{name}")
+  void deleteStudent(String name);
+}
