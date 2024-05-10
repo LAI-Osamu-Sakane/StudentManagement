09026c14a07d15316fd10c537a1b4ae3562af652
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2024-05-10 10:05:25 +0900

    33回終了後

--- src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
+++ src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
-public class StudentsCourses {  //ファイル自体課題23で作成
+public class StudentsCourses {
 
   private String courseId;
-  private String studentsId;
+  private String studentId;
   private String courseName;
   private LocalDateTime startingDate;
   private LocalDateTime scheduledEndDate;
-
 }
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -2,8 +2,11 @@ package raisetech.StudentManagement.repository;
 
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
@@ -23,7 +26,35 @@ public interface StudentRepository {
   @Select("SELECT * FROM students")
   List<Student> search();
 
-  @Select("SELECT * FROM students_courses") //課題23で作成
-  List<StudentsCourses> searchStudentsCourses();  //課題23で作成
+  @Select("SELECT * FROM students where studentId = #{studentId}")
+  Student searchStudent(String studentId);
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchStudentsCoursesList();
+
+  @Select("SELECT * FROM students_courses WHERE studentId = #{studentId}")
+  List<StudentsCourses> searchStudentCourse(String studentId);
+
+  // INSERT文の書き方注意
+  @Insert("INSERT INTO students (name, kana, nickname, mail, area, age, sex, remark, is_deleted)"
+      + " VALUE(#{name}, #{kana}, #{nickname}, #{mail}, #{area}, #{age}, #{sex}, #{remark}, false)")
+  //MyBatis用の設定、studentIdについて
+  @Options(useGeneratedKeys = true,keyProperty = "studentId")
+  void registerStudent(Student student);
+
+  @Insert("INSERT INTO students_courses (studentId, course_name, starting_date, scheduled_end_date)"
+      + " VALUE(#{studentId}, #{courseName}, #{startingDate}, #{scheduledEndDate})")
+  @Options(useGeneratedKeys = true,keyProperty = "courseId")
+  void registerStudentCourses(StudentsCourses studentsCourse);
+
+  // INSERT文の書き方注意
+  @Update("UPDATE students Set name = #{name}, kana = #{kana}, nickname = #{nickname},"
+      + " mail = #{mail}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{deleted} WHERE studentId = #{studentId}")
+  //MyBatis用の設定、studentIdについて
+  void updateStudent(Student student);
+
+  @Update("UPDATE students_courses set course_name = #{courseName} WHERE courseId = #{courseId} ")
+  void updateStudentCourses(StudentsCourses studentsCourse);
 
 }
+
